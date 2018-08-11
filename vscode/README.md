a565994904ff82885fbf9e79b6835a7870717d08
Denis Orehovsky <denis.orehovsky@gmail.com>
2018-08-11 11:53:49 +0300

    Add vscode to the dotfiles

---
+++ vscode/README.md
@@ -0,0 +1,14 @@
+VSCode
+======
+
+### Installation
+
+```bash
+# Install extensions
+sh ~/.dotfiles/vscode/install.sh
+
+# Symlink settings
+ln -s ~/.dotfiles/vscode/settings.symlink ~/.config/Code/User/settings.json
+
+```
+
---
+++ vscode/install.sh
@@ -0,0 +1,7 @@
+code --install-extension EditorConfig.EditorConfig
+code --install-extension vscodevim.vim
+code --install-extension ms-python.python
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension mjmcloug.vscode-elixir
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension teabyii.ayu
