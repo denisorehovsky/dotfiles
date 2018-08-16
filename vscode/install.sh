cda1f5d9a66774eccd1b63a654e80391c6d6f210
Denis Orehovsky <denis.orehovsky@gmail.com>
2018-08-16 10:10:09 +0300

    Install gitlens

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,4 +4,5 @@ code --install-extension ms-python.python
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension mjmcloug.vscode-elixir
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension eamodio.gitlens
 code --install-extension teabyii.ayu
