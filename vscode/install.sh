729a1b45788f7bb7898f705c013b5ae399189151
Denis Orehovsky <denis.orehovsky@gmail.com>
2019-06-05 11:54:20 +0300

    Remove mypy and add pyright

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,5 +1,6 @@
 code --install-extension EditorConfig.EditorConfig
 code --install-extension ms-python.python
+code --install-extension ms-pyright.pyright
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension octref.vetur
 code --install-extension mjmcloug.vscode-elixir
